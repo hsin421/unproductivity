20a0ba860d723f7ad11e8fbc6066a23c84b6d74e
Hsin <shinsonh@gmail.com>
2014-08-26 13:45:50 -0400

    for heroku 2

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20140731192156) do
+  enable_extension "plpgsql"
 
   create_table "content_providers", force: true do |t|
     t.string   "name"
